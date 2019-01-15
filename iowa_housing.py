9b78a4b79e5c7dea8ad67888ad32d02952a8f093
Wyatt Hoodes <hoodesw@hawaii.edu>
2019-01-14 14:13:44 -1000

    First model with sklearn

--- iowa_housing.py
+++ iowa_housing.py
@@ -1,3 +1,4 @@
+from sklearn.tree import DecisionTreeRegressor
 import pandas as pd
 
 # Exercise 1: Explore your data
@@ -9,5 +10,22 @@ pd.set_option('display.max_columns', None)
 iowa_file_path = '~/datasets/housing-kaggle/train.csv'
 iowa_data = pd.read_csv(iowa_file_path)
 
-# Examination
-print(iowa_data.describe())
+# If data examination is so desired
+iowa_data.describe()
+iowa_data.columns
+
+y = iowa_data.SalePrice
+
+iowa_features = ['BedroomAbvGr', 'LotArea', 'GarageArea', 'GrLivArea', 'FullBath']
+X = iowa_data[iowa_features]
+actual = iowa_data['SalePrice']
+
+iowa_model = DecisionTreeRegressor(random_state=1)
+iowa_model.fit(X, y)
+
+print("Making predictions for the following five houses: ")
+print(X.head())
+print("The predictions: ")
+print(iowa_model.predict(X.head()))
+print("\nActual: ")
+print(actual.head())
